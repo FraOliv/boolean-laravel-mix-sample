8953613330df0c0fb2bd5df2d1dc097c95056758
Ottavio Fogliata <5075650+ott-fogliata@users.noreply.github.com>
2021-02-17 09:49:48 +0100

    css url fix

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,8 @@
 let mix = require('laravel-mix');
 
-mix.sass('src/app.scss', 'dist/')
+mix
+  .sass('src/app.scss', 'dist/')
+  .options({ processCssUrls: false });
 
 // mix
 //   .sass('src/app.scss', 'dist/')
